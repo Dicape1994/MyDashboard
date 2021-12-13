cdb09b559c78e149f08cec953c0b6e9b621771ed
joseph <joseph.tong-mboa@epitech.eu>
2021-12-13 17:10:54 +0100

    Initial commit

---
+++ src/test/java/com/dashboard/mydashboard/MyDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.mydashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
